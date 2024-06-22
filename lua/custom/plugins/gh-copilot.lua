9edcfa87f9a9ae751e4656b0113632d9fcf296a2
Andrew Jaffie <andrew@ajaffie.dev>
2024-06-22 14:28:10 -0400

    fix: Fix tab key

--- lua/custom/plugins/gh-copilot.lua
+++ lua/custom/plugins/gh-copilot.lua
@@ -2,11 +2,21 @@ return {
   'zbirenbaum/copilot.lua',
   event = 'InsertEnter',
   cmd = 'Copilot',
-  opts = {
-    suggestion = {
-      keymap = {
-        accept = '<Tab>',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        keymap = {
+          accept = false,
+        },
       },
-    },
-  },
+    }
+    vim.keymap.set('i', '<Tab>', function()
+      local suggestion = require 'copilot.suggestion'
+      if suggestion.is_visible() then
+        suggestion.accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+      end
+    end, { desc = 'Super Tab' })
+  end,
 }
